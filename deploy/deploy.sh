bf40ddb991d7b0ba6814baccb0faf9aa80f0e65e
Steven Tyson <steventyson@Stevens-MBP.lan>
2019-06-15 10:37:18 +1000

    added cognito lambda function

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,12 +1,12 @@
-# run me from the deploy folder
-aws cloudformation validate-template --template-body file://../cf/s3.yaml
-aws cloudformation validate-template --template-body file://../cf/lambda.s3.yaml
-aws cloudformation validate-template --template-body file://../cf/cognito.yaml
-
 aws cloudformation deploy \
     --stack-name s3-lambda \
     --tags $(cat tags.properties) \
     --template-file ../cf/lambda.s3.yaml
+
+# run me from the deploy folder
+aws cloudformation validate-template --template-body file://../cf/s3.yaml
+aws cloudformation validate-template --template-body file://../cf/lambda.s3.yaml
+aws cloudformation validate-template --template-body file://../cf/cognito.yaml
 
 cd ../sam-app-s3
 make build
@@ -16,15 +16,22 @@ aws s3 cp s3copy.zip s3://lambda-849375858678/s3copy.zip
 cd ../../deploy/
 
 aws cloudformation deploy \
-    --stack-name s3-unicorn-rentals \
+    --stack-name unicorn-rentals-s3 \
     --tags $(cat tags.properties) \
-    --parameter-overrides $(cat s3-unicorn-rentals-s3.properties) \
+    --parameter-overrides $(cat unicorn-rentals-s3.properties) \
     --template-file ../cf/s3.yaml \
     --capabilities CAPABILITY_NAMED_IAM
 
+cd ../sam-app-cognito
+make build
+cd cognito
+zip cognito.zip cognito
+aws s3 cp cognito.zip s3://lambda-849375858678/cognito.zip
+cd ../../deploy/
+
 aws cloudformation deploy \
-    --stack-name s3-unicorn-cognito \
+    --stack-name unicorn-rentals-cognito \
     --tags $(cat tags.properties) \
-    --parameter-overrides $(cat s3-unicorn-rentals-cognito.properties) \
+    --parameter-overrides $(cat unicorn-rentals-cognito.properties) \
     --template-file ../cf/cognito.yaml \
     --capabilities CAPABILITY_NAMED_IAM
--- sam-app-cognito/cognito/cognito.go
+++ sam-app-cognito/cognito/cognito.go
@@ -1,6 +1,3 @@
-// take imported values from CF as event properties
-// replace values inside xs
-//push to s3
 package main
 
 import (
@@ -9,21 +6,31 @@ import (
 	"log"
 	"strings"
 
+	"github.com/aws/aws-lambda-go/cfn"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-
-	"github.com/aws/aws-lambda-go/cfn"
-	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(ctx context.Context, event cfn.Event) {
+
+	userPool := event.ResourceProperties["UserPool"]
+	us := userPool.(string)
+	client := event.ResourceProperties["Client"]
+	cs := client.(string)
+	bucket := event.ResourceProperties["Bucket"]
+	bs := bucket.(string)
+	region := event.ResourceProperties["Region"]
+	rs := region.(string)
+
 	xs := `
 	window._config = {
 		cognito: {
 			userPoolId: 'replacemeuserPoolId', // e.g. us-east-2_uXboG5pAb
 			userPoolClientId: 'replacemeuserPoolClientId', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-			region: 'ap-southeast-2' // e.g. us-east-2
+			region: 'replacemeregion' // e.g. us-east-2
 		},
 		api: {
 			invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
@@ -31,25 +38,34 @@ func handler(ctx context.Context, event cfn.Event) {
 	};
 	`
 
-	xs1 := strings.Replace(xs, "replacemeuserPoolId", "STEVEISHERE", 1)
-	xs1 = strings.Replace(xs1, "replacemeuserPoolClientId", "BELIEVEINME", 1)
-
-	xs1nr := strings.NewReader(xs1)
-	fmt.Printf("%T\n", xs1nr)
-	fmt.Println(xs1nr)
+	xs1 := strings.Replace(xs, "replacemeuserPoolId", us, 1)
+	xs1 = strings.Replace(xs1, "replacemeuserPoolClientId", cs, 1)
+	xs1 = strings.Replace(xs1, "replacemeregion", rs, 1)
 
 	svc := s3.New(session.New())
 	input := &s3.PutObjectInput{
 		Body:   aws.ReadSeekCloser(strings.NewReader(xs1)),
-		Bucket: aws.String("wildrydes-849375858678"),
-		Key:    aws.String("sausage.js"),
+		Bucket: aws.String(bs),
+		Key:    aws.String("js/config.js"),
 	}
 
 	result, err := svc.PutObject(input)
 	if err != nil {
-		log.Print("SHIT THE BED")
+		log.Print(err)
+	} else {
+		fmt.Println(result)
 	}
-	fmt.Println(result)
+
+	r := cfn.NewResponse(&event)
+	r.Status = "SUCCESS"
+	if r.PhysicalResourceID == "" {
+		r.PhysicalResourceID = (lambdacontext.LogStreamName)
+	}
+
+	er := r.Send()
+	if er != nil {
+		fmt.Println(er)
+	}
 
 }
 
