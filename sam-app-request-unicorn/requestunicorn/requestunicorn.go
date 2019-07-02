79a8e43451a91709f2d2d6f276a98270fecf6f3c
steven tyson <steven.tyson@blah>
2019-07-02 22:24:21 +1000

    next fix to match node request

--- sam-app-request-unicorn/requestunicorn/requestunicorn.go
+++ sam-app-request-unicorn/requestunicorn/requestunicorn.go
@@ -1,6 +1,12 @@
+//https://github.com/ianfoo/aws-lambda-go-demo/blob/master/main.go
+//https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-using-start-api.html
+//https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-create-api-as-simple-proxy-for-lambda.html#api-gateway-create-api-as-simple-proxy-for-lambda-test
+
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"math/rand"
 
@@ -14,11 +20,24 @@ type unicorn struct {
 	Gender string
 }
 
-//letters to use in the ranfom string generator
+type unicornRequest struct {
+	PickupLocation struct {
+		Latitude  float64 `json:"Latitude"`
+		Longitude float64 `json:"Longitude"`
+	} `json:"PickupLocation"`
+}
+
+//letters to use in the random string generator
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
 
+//(events.APIGatewayProxyResponse, error)
+
 //entry point for lambda
-func handler(request events.APIGatewayProxyRequest) {
+//context always comes first when running a lambda handler
+//then the request follows
+//context is the
+//(events.APIGatewayProxyResponse, error)
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) {
 
 	//create unicorns based upon the unicorn struct type
 	u1 := unicorn{"Bucephalus", "Golden", "Male"}
@@ -33,14 +52,32 @@ func handler(request events.APIGatewayProxyRequest) {
 		fmt.Println("AUTHORIZER NOT PRESENT")
 	}
 
-	fmt.Println(request.RequestContext.Authorizer)
-
 	//make a slice of bytes 24 characters long from the const letterbytes
-	b := make([]byte, 24)
-	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	numRideID := make([]byte, 24)
+	for i := range numRideID {
+		numRideID[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
-	fmt.Println(string(b))
+	rideID := string(numRideID)
+	fmt.Println(string(rideID))
+	// fmt.Println("RECEIVED EVENT - RIDE ID IS:", rideID, "EVENT ID IS", request)
+	// fmt.Println("ONTEXT IS", ctx)
+
+	//Get the username for the session from cognito
+	username := request.RequestContext.Authorizer["claims"] //print this in json2go to see if you can get more?
+	fmt.Println(username)
+
+	// fmt.Println("REQUEST BODY IS", request.Body)
+
+	//start parsing the request body
+	var req unicornRequest
+
+	//unmarshall the shit json into something go can use
+	err := json.Unmarshal([]byte(request.Body), &req)
+	if err != nil {
+		fmt.Println("HAD PROBLEMS UNMARSHALING", err)
+	}
+
+	fmt.Println(req.PickupLocation)
 
 }
 
